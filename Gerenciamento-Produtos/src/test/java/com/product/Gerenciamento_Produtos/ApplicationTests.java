72e163ec24e20b9cbf12b15a79f7468d0ffaad83
Prevs-997 <mthsnetwork@gmail.com>
2026-03-18 19:00:41 -0300

    Models Criados
    Adicionado dependência do swagger na hora de baixar o spring.

---
+++ Gerenciamento-Produtos/src/test/java/com/product/Gerenciamento_Produtos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.Gerenciamento_Produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
